902592b1e037a3bb306e6b09ca9e8a9efc32ce20
JULIO NILSON <julio.nilson11@gmail.com>
2026-06-11 12:31:11 -0300

    Change input types from reg to wire in display module

--- modules/displays.v
+++ modules/displays.v
@@ -1,9 +1,9 @@
 module display(
     input clock,
-    input reg [5:0] top_card,
-    input reg [5:0] player_card,
-    input reg [6:0] n_player,
-    input reg [6:0] n_cpu,
+    input [5:0] top_card,
+    input [5:0] player_card,
+    input [6:0] n_player,
+    input [6:0] n_cpu,
 output reg [6:0] hex7,
 output reg [6:0] hex6,
 output reg [6:0] hex5,
@@ -96,11 +96,11 @@ always @(*)
         default: hex5 = 7'b1111111;
     endcase case(cor_top)
      // O zero será vermelho, o 1 será verde, o  2 será amarelo e o 3 será azul 
-        2'd0: hex4: 7'b1001110;
-        2'd1: hex4: 7'b0010000;
-        2'd2: hex4: 7'b0010001;
-        2'd3: hex4: 7'b0000011;
-        default: hex4: 7'b1111111;
+        2'd0: hex4 = 7'b1001110;
+        2'd1: hex4 = 7'b0010000;
+        2'd2: hex4 = 7'b0010001;
+        2'd3: hex4 = 7'b0000011;
+        default: hex4 = 7'b1111111;
     endcase case(n_card_player)
          4'd0: hex3  = 7'b1000000;
         4'd1: hex3 = 7'b1111001;
@@ -110,8 +110,8 @@ always @(*)
         4'd5: hex3 = 7'b0010010;
         4'd6: hex3 = 7'b0000010;
         4'd7: hex3 = 7'b1111000;
-        4'd8: hex3 = 7'b0000000;
-        4'd9: hex3 = 7'b0010000;
+        4'd8: hex3 = 7'b0000000; 
+        4'd9: hex3 = 7'b0011000;
         4'd10: hex3 = 7'b0111111; // Skip (Traço no meio)
         4'd11: hex3 = 7'b0000111; // Reverse (Letra 't')
         4'd12: hex3 = 7'b0100001; // Draw Two (Letra 'd')
@@ -119,11 +119,11 @@ always @(*)
         4'd14: hex3 = 7'b0001110; // Wild Draw Four (Letra 'F')
         default: hex3 = 7'b1111111;
     endcase case(cor_player)
-        2'd0: hex2: 7'b1001110;
-        2'd1: hex2: 7'b0010000;
-        2'd2: hex2: 7'b0010001;
-        2'd3: hex2: 7'b0000011;
-        default: 7'b1111111;
+        2'd0: hex2 = 7'b1001110;
+        2'd1: hex2 = 7'b0010000;
+        2'd2: hex2 =  7'b0010001;
+        2'd3: hex2 = 7'b0000011;
+        default: hex2 = 7'b1111111;
     endcase
 
 
